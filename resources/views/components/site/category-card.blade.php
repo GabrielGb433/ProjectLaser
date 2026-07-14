c76c71d5832eb094999a37a7fd05cefc4f8f975e
Gabriel Batista <seuemail@example.com>
2026-07-14 15:43:14 -0300

    Move nome das categorias acima das imagens

--- resources/views/components/site/category-card.blade.php
+++ resources/views/components/site/category-card.blade.php
@@ -6,6 +6,11 @@
 @endphp
 
 <a class="category-card reveal" href="{{ route('fotos.index', ['categoria' => $categoria->slug]) }}" data-reveal>
+    <span class="category-content">
+        <strong>{{ $categoria->nome }}</strong>
+        <span>{{ $quantidade }} {{ $quantidade === 1 ? 'projeto' : 'projetos' }}</span>
+    </span>
+
     <span class="category-media">
         @if ($imagem)
             <img src="{{ asset('storage/'.ltrim($imagem, '/')) }}" alt="{{ $categoria->nome }}" loading="lazy">
@@ -13,10 +18,5 @@
             <span class="category-placeholder" aria-hidden="true">{{ mb_substr(trim($categoria->nome), 0, 1) }}</span>
         @endif
         <span class="category-arrow"><x-site.icon name="arrow-up-right" /></span>
-    </span>
-
-    <span class="category-content">
-        <strong>{{ $categoria->nome }}</strong>
-        <span>{{ $quantidade }} {{ $quantidade === 1 ? 'projeto' : 'projetos' }}</span>
     </span>
 </a>
